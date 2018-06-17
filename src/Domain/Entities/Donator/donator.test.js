cc8cef971e41e6233cb7ae1551bd7c6184760cb0
Marcos Vinícius C. da Silva <marvinc.silva@gmail.com>
2018-06-17 13:07:03 -0300

    remove not-so-usefull test

--- src/Domain/Entities/Donator/donator.test.js
+++ src/Domain/Entities/Donator/donator.test.js
@@ -3,37 +3,27 @@ const assert = require('assert')
 const Donator = require('./')
 
 describe('Donator', () => {
-  context('Valid Donator', () => {
-    const donator = new Donator({ hairLength: 6, hairStatus: Donator.HairStatuses.Healthy })
+  context('Donator has short hair', () => {
+    const donator = new Donator({ hairLength: 2, hairStatus: Donator.HairStatuses.Healthy })
 
-    it('Has no errors', () => assert.deepStrictEqual(donator.errors, []))
+    it('Has a "short hair" error', () => assert.deepStrictEqual(donator.errors, ['hair too short']))
 
-    it('Flags as valid', () => assert.equal(donator.isValid, true))
+    it('Flags as invalid', () => assert.equal(donator.isValid, false))
   })
 
-  context('Invalid Donator', () => {
-    describe('Short hair', () => {
-      const donator = new Donator({ hairLength: 2, hairStatus: Donator.HairStatuses.Healthy })
+  context('Donator has long hair', () => {
+    const donator = new Donator({ hairLength: 16, hairStatus: Donator.HairStatuses.Healthy })
 
-      it('Has a "short hair" error', () => assert.deepStrictEqual(donator.errors, ['hair too short']))
+    it('Has a "long hair" error', () => assert.deepStrictEqual(donator.errors, ['hair too long']))
 
-      it('Flags as invalid', () => assert.equal(donator.isValid, false))
-    })
+    it('Flags as invalid', () => assert.equal(donator.isValid, false))
+  })
 
-    describe('Long hair', () => {
-      const donator = new Donator({ hairLength: 16, hairStatus: Donator.HairStatuses.Healthy })
+  context('Donator has unhealthy hair', () => {
+    const donator = new Donator({ hairLength: 6, hairStatus: Donator.HairStatuses.Unhealthy })
 
-      it('Has a "long hair" error', () => assert.deepStrictEqual(donator.errors, ['hair too long']))
+    it('Has an "unhealthy hair" error', () => assert.deepStrictEqual(donator.errors, ['unhealthy hair']))
 
-      it('Flags as invalid', () => assert.equal(donator.isValid, false))
-    })
-
-    describe('Unhealthy hair', () => {
-      const donator = new Donator({ hairLength: 6, hairStatus: Donator.HairStatuses.Unhealthy })
-
-      it('Has an "unhealthy hair" error', () => assert.deepStrictEqual(donator.errors, ['unhealthy hair']))
-
-      it('Flags as invalid', () => assert.equal(donator.isValid, false))
-    })
+    it('Flags as invalid', () => assert.equal(donator.isValid, false))
   })
 })
